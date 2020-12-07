8e99103e95eb1c61b9b92c2c6ffa9264b7ac5e4a
Sydney James <sydneyjames7@gmail.com>
2020-12-07 14:12:55 -0800

    initial commit

---
+++ sql/setup.sql
@@ -0,0 +1,7 @@
+CREATE TABLE dawsons_creek_characters (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    character_name TEXT NOT NULL,
+    real_life_name TEXT NOT NULL,
+    description TEXT,
+    sydney_rating NUMBER
+);
