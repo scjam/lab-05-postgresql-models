9c12a38f9c5fd94dc969e98bb637a92ce66cce98
Sydney James <sydneyjames7@gmail.com>
2020-12-08 15:31:56 -0800

    tests written, getting ready to test

--- sql/setup.sql
+++ sql/setup.sql
@@ -5,3 +5,43 @@ CREATE TABLE dawsons_creek_characters (
     description TEXT,
     sydney_rating INT
 );
+
+CREATE TABLE titanic_characters (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    character_name TEXT NOT NULL,
+    real_life_name TEXT NOT NULL,
+    based_on_real_life_person BOOLEAN,
+    hot_factor INT
+);
+
+CREATE TABLE spice_girls (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    spice_girl_name TEXT NOT NULL,
+    real_life_name TEXT NOT NULL,
+    signature TEXT,
+    best_quote_from_spice_world TEXT
+);
+
+CREATE TABLE rom_coms (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    lead_character TEXT NOT NULL,
+    love_interest TEXT,
+    level_of_believability INT
+);
+
+CREATE TABLE rom_coms (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    lead_character TEXT NOT NULL,
+    love_interest TEXT,
+    degree_of_believability INT
+);
+
+CREATE TABLE wes_anderson_movies (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    is_there_bill_murray BOOLEAN NOT NULL,
+    character_most_likely_to_be_replicated_for_halloween TEXT,
+    location TEXT
+);
--- tests/index.test.js
+++ tests/index.test.js
@@ -2,29 +2,103 @@ require('dotenv').config();
 const request = require('supertest');
 const app = require('..');
 
-describe('routes', () => {
-  it.skip('inserts a new line into Dawsons Creek Table', async() => {
+describe('endpoints', () => {
+  test.skip('posts a new row to Dawsons Creek table', async() => {
     const post = {
-      characterName: 'Joey',
-      realLifeName: 'Katie Holmes',
-      description: 'Sarcastic, smug, sexy',
-      sydneyRating: 90
+      characterName: 'Dawson',
+      realLifeName: 'James Van Der Beek',
+      description: 'Cry baby',
+      sydneyRating: 30
     };
-
     const expectation = {
-      id: '9',
-      characterName: 'Joey',
-      realLifeName: 'Katie Holmes',
-      description: 'Sarcastic, smug, sexy',
-      sydneyRating: 90
+      id: '2',
+      characterName: 'Dawson',
+      realLifeName: 'James Van Der Beek',
+      description: 'Cry baby',
+      sydneyRating: 30
     };
-
     const data = await request(app)
       .post('/dawsons')
       .send(post)
       .expect('Content-Type', /json/)
       .expect(200);
-    
+    expect(data.body).toEqual(expectation);
+  });
+
+  test.skip('gets all rows from Dawsons Creek table', async() => {
+    const expectation = [
+      {
+        id: '1',
+        characterName: 'Pacey',
+        realLifeName: 'Joshua Jackson',
+        description: 'Full of himself',
+        sydneyRating: 72
+      },
+      {
+        id: '2',
+        characterName: 'Dawson',
+        realLifeName: 'James Van Der Beek',
+        description: 'Cry baby',
+        sydneyRating: 30
+      }
+    ];
+    const data = await request(app)
+      .get('/dawsons/')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(expectation);
+  });
+
+  test.skip('get character by id', async() => {
+    const expectation = {
+      id: '1',
+      characterName: 'Pacey',
+      realLifeName: 'Joshua Jackson',
+      description: 'Full of himself',
+      sydneyRating: 72
+    };
+    const data = await request(app)
+      .get('/dawsons/1')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(expectation);
+  });
+
+  test.skip('updates a character by id', async() => {
+    const update = {
+      characterName: 'Pacey Witter',
+      realLifeName: 'Joshua Jackson',
+      description: 'Dates his teacher',
+      sydneyRating: 76
+    };
+    const expectation = {
+      id: '1',
+      characterName: 'Pacey Witter',
+      realLifeName: 'Joshua Jackson',
+      description: 'Dates his teacher',
+      sydneyRating: 76
+    };
+    const data = await request(app)
+      .put('/dawsons/1')
+      .send(update)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(expectation);
+  });
+
+  test.skip('deletes a character', async() => {
+    const expectation = {
+      id: '2',
+      characterName: 'Dawson',
+      realLifeName: 'James Van Der Beek',
+      description: 'Cry baby',
+      sydneyRating: 30
+    };
+    const data = await request(app)
+      .delete('/dawsons/2')
+      .expect('Content-Type', /json/)
+      .expect(200);
+
     expect(data.body).toEqual(expectation);
   });
 });
